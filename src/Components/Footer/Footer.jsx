698f7b0dc20d59b721d78944d2dfe4570ed1d916
Mahkamov9 <ahadjonmahkamov111@gmail.com>
2024-10-26 18:13:18 +0500

    footer end

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,47 @@
 import React from 'react'
 import "./Footer.css"
 
+import Logo from "../../assets/Images/bnp_logo.png"
+import { Link } from 'react-router-dom'
+import { IoMailOpen } from "react-icons/io5";
 
 export default function Footer() {
   return (
     <>
       <footer>
         <div className="container">
-          <h4>Footer</h4>
+          <div className="footer_list">
+            <div className='footer_item'>
+              <img className='footer_logo' src={Logo} alt="Logo" />
+              <h4>“Bukhara Natural Product” koʻp yillardan buyon butun dunyoda foydalanish uchun paxta matolari ishlab chiqaradigan kompaniya hisoblanadi.</h4>
+            </div>
+            <div className='footer_item'>
+              <Link className='footer_link  footerHead_link'>Menyu</Link>
+              <Link className='footer_link'>Commit</Link>
+              <Link className='footer_link'>Commit</Link>
+              <Link className='footer_link'>Commit</Link>
+              <Link className='footer_link'>Commit</Link>
+            </div>
+            <div className='footer_item'>
+              <Link className='footer_link  footerHead_link'>Menyu</Link>
+              <Link className='footer_link'>Commit</Link>
+              <Link className='footer_link'>Commit</Link>
+              <Link className='footer_link'>Commit</Link>
+              <Link className='footer_link'>Commit</Link>
+            </div>
+            <div className='footer_item'>
+              <Link className='footer_link footerHead_link'>Menyu</Link>
+              <div className="footerInput_box">
+                <input className='footer_input' type="text" placeholder='examle@gmail.com' />
+                <IoMailOpen className='footerInput_icon' />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer_bottom">
+          <div className="container">
+            <h3>© 2024 MChJ "BUXORA NATURAL PRODUCT". Barcha huquqlar himoyalangan. | Mahkamov</h3>
+          </div>
         </div>
       </footer>
     </>
