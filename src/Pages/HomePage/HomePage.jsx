542df8c37ade33c1ea65806dcc44617f0395d5e3
Mahkamov9 <ahadjonmahkamov111@gmail.com>
2024-10-27 23:46:02 +0500

    header end res

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import './HomePage.css';
+import headerSofa from "../../assets/Images/headerSofa.png";
+import { useTranslation } from 'react-i18next';
+
 
 export default function HomePage() {
+
+  const {t, i18n} = useTranslation()
   return (
     <>
-      <div className="container">
-      <h1>HomePage</h1>
-      </div>
+      <section>
+        <div className="header">
+          <div className="container">
+            <div className="header_box">
+              <img className='header_box_img' src={headerSofa} alt="Mahkamov" />
+              <div className='header_box_content'>
+                <p>{t("buxoro")}</p>
+                <p>{t("tabiy")}</p>
+                <p>{t("product")}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
