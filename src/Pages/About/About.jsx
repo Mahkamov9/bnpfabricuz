ffe278bff6f5551ff40cb7c4dd314c683d43db37
Mahkamov9 <ahadjonmahkamov111@gmail.com>
2024-10-29 18:07:42 +0500

    about page end

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import "./About.css";
 
+import aboutimg from "../../assets/Images/aboutimg.png"
+
 
 export default function About() {
   return (
-    <div>About</div>
+    <section>
+      <div className="about_page">
+        <div className="container">
+          <div className="about_box">
+            <div className="about_content">
+              <h1>"Bukhara Natural Product"</h1>
+              <p>Our factory has been a company that produces cotton fabrics for use all over the world for many years.</p>
+              <p>Our factory has been a company that produces cotton fabrics for use all over the world for many years.</p>
+              <p>Our factory has been a company that produces cotton fabrics for use all over the world for many years.</p>
+              <p>Our factory has been a company that produces cotton fabrics for use all over the world for many years.</p>
+              <p>Our factory has been a company that produces cotton fabrics for use all over the world for many years.</p>
+              <p>Our factory has been a company that produces cotton fabrics for use all over the world for many years.</p>
+            </div>
+            <div className="about_images">
+              <img src={aboutimg} alt="Mahkamov" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
